33715c70fe016f65fcf0a4a6d2005b4526af75e6
yoouyeon <jyeon.yoon59@gmail.com>
2023-01-12 11:32:30 +0900

    🧪 test : intra test 조금 추가

--- tests/intra_main.cpp
+++ tests/intra_main.cpp
@@ -58,6 +58,7 @@ int main(int argc, char** argv) {
 	const int seed = atoi(argv[1]);
 	srand(seed);
 
+	std::cout << "TEST START!\n";
 	clock_t time = clock();
 	ft::vector<std::string> vector_str;
 	ft::vector<int> vector_int;
@@ -66,18 +67,22 @@ int main(int argc, char** argv) {
 	ft::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
 	ft::map<int, int> map_int;
 
+	std::cout << "\nVECTOR TEST ... \n";
+	std::cout << "\n- push_back\n";
 	for (int i = 0; i < COUNT; i++)
 	{
 		vector_buffer.push_back(Buffer());
 	}
-
+	std::cout << "\n- index operator\n";
 	for (int i = 0; i < COUNT; i++)
 	{
 		const int idx = rand() % COUNT;
 		vector_buffer[idx].idx = 5;
 	}
+	std::cout << "\n- swap (empty)\n";
 	ft::vector<Buffer>().swap(vector_buffer);
 
+	std::cout << "\n- at\n";
 	try
 	{
 		for (int i = 0; i < COUNT; i++)
@@ -90,13 +95,17 @@ int main(int argc, char** argv) {
 	catch(const std::exception& e)
 	{
 		//NORMAL ! :P
+		std::cout << "This vector is empty\n";
 	}
 	
+	std::cout << "\nMAP TEST ... \n";
+	std::cout << "\n- insert, ft::pair\n";
 	for (int i = 0; i < COUNT; ++i)
 	{
 		map_int.insert(ft::make_pair(rand(), rand()));
 	}
 
+	std::cout << "\n- operator[]\n";
 	int sum = 0;
 	for (int i = 0; i < 10000; i++)
 	{
@@ -104,10 +113,11 @@ int main(int argc, char** argv) {
 		sum += map_int[access];
 	}
 	std::cout << "should be constant with the same seed: " << sum << std::endl;
-
+	std::cout << "\n- assign operator\n";
 	{
 		ft::map<int, int> copy = map_int;
 	}
+	std::cout << "\nSTACK TEST ... \n";
 	MutantStack<char> iterable_stack;
 	for (char letter = 'a'; letter <= 'z'; letter++)
 		iterable_stack.push(letter);
@@ -117,7 +127,7 @@ int main(int argc, char** argv) {
 	}
 	std::cout << std::endl;
 	time = clock() - time;
-	std::cout << "time : " << time / CLOCKS_PER_SEC << std::endl;
-	system("leaks test_intra");
+	std::cout << "\ntime : " << time / CLOCKS_PER_SEC << std::endl;
+	// system("leaks test_intra");
 	return (0);
 }
